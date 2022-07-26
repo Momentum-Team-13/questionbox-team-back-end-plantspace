fda13f04da2e93a8cc46a6e125ecc9756bd6d785
Yuriy Golota <ypgolota@pm.me>
2022-07-26 12:53:32 -0400

    created CustomUser & BaseModel models, not tested yet

--- api/models.py
+++ api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    def __str__(self):
+        return self.username
+
+    def __repr__(self):
+        return f'<User username={self.username} pk={self.pk}>'
+
+class BaseModel(models.Model):
+    # Both attributes need options
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
