f7992e233bbb71b11b76ce185cfcaace2ba71494
Yuriy Golota <ypgolota@pm.me>
2022-07-27 11:39:08 -0400

    add endpoints table to README and add '/' to url path

--- core/urls.py
+++ core/urls.py
@@ -24,5 +24,5 @@ urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
     path('api/questions/', api_views.QuestionListView.as_view(), name='all_questions'),
-    path('api/questions/new', api_views.QuestionCreateView.as_view(), name='new_question'),
+    path('api/questions/new/', api_views.QuestionCreateView.as_view(), name='new_question'),
 ]
