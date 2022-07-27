b59e290c874dae57298f7fd1b926c65484e1004f
Amanda McMullin <amanda.mcmullin@gmail.com>
2022-07-27 11:56:50 -0400

    Corrected typos and worked on permissions.py

--- api/permissions.py
+++ api/permissions.py
@@ -1,9 +1,11 @@
-from rest_framework import permissions
+# from rest_framework import permissions
 
-class IsOwner(permissions.BasePermission):
-    """
-    Custom permission that requires editor of object to be the object owner.
-    """
 
-    def has_object_permission(self, request, view, obj):
-        return obj.user == request.user
+# class IsOwner(permissions.BasePermission):
+#     """
+#     Custom permission that requires editor of object to be the object owner.
+#     """
+
+#     def has_object_permission(self, request, view, obj):
+#         # Write permissions are only allowed to the owner of the object.
+#         return obj.user == request.user
