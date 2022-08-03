8f20430cb8d5dd3ff2749bc25524c2d09db62ca1
Yuriy Golota <ypgolota@pm.me>
2022-08-02 22:15:40 -0400

    fix merge conflicts from myquestions endpoint

--- api/serializers.py
+++ api/serializers.py
@@ -2,23 +2,26 @@ from rest_framework import serializers
 from .models import Answer, User, Question
 
 
-class QuestionSerializer(serializers.ModelSerializer):
-    user = serializers.ReadOnlyField(source='user.username')
-    category = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Question
-        fields = ('__all__')
-
-    def get_category(self, obj):
-        return obj.get_category_display()
-
-
 class UserSerializer(serializers.ModelSerializer):
+    answers = 'AnswerSerializer'(many=True, read_only=True)
+    questions = 'QuestionSerializer'(many=True, read_only=True)
 
     class Meta:
         model = User
-        fields = ('id', 'username')
+        fields = ('id', 'username', 'questions', 'answers')
+
+
+class QuestionSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source='user.username')
+    category_name = serializers.SerializerMethodField()
+    answers = 'AnswerSerializer'(many=True, read_only=True)
+    
+    class Meta:
+        model = Question
+        fields = ('__all__')
+    
+    def get_category_name(self, obj):
+        return obj.get_category_display()
 
 
 class AnswerSerializer(serializers.ModelSerializer):
@@ -30,24 +33,6 @@ class AnswerSerializer(serializers.ModelSerializer):
         fields = ('pk', 'user', 'answer_body', 'created_at')
 
 
-class QuestionSerializer(serializers.ModelSerializer):
-    user = serializers.ReadOnlyField(source='user.username')
-    category_name = serializers.SerializerMethodField()
-    answers = AnswerSerializer(many=True, read_only=True)
-    
-    class Meta:
-        model = Question
-        fields = ('__all__')
-    
-    def get_category_name(self, obj):
-        return obj.get_category_display()
 
 
-class UserSerializer(serializers.ModelSerializer):
-    answers = AnswerSerializer(many=True, read_only=True)
-    questions = QuestionSerializer(many=True, read_only=True)
-
-    class Meta:
-        model = User
-        fields = ('id', 'username', 'questions', 'answers')
 
